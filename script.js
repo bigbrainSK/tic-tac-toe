26eb079e0cc870bc52539566c3b62228a1103f51
Shahzaib Khan <shahzaibkhanisme@gmail.com>
2023-12-03 22:09:49 -0800

    fixed all previous bugs, working fine now

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const cells = document.querySelectorAll('.cell');
-const restartButton = document.querySelector('#restartButton');
-const gameStatus = document.querySelector('#gameStatus');
-const winCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [0,4,8]
-];
 const Gameboard = (() => {
     let gameboard = ['', '', '', '', '', '', '', '', '',];
     let currentPlayer = 'X';
     let running = false; 
+
+    const cells = document.querySelectorAll('.cell');
+    const restartButton = document.querySelector('#restartButton');
+    const gameStatus = document.querySelector('#gameStatus');
+    const winCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8]
+    ];
 
     initialize();
 
@@ -34,62 +35,53 @@ const Gameboard = (() => {
 
         updateCell(this, cellIndex);                         //OTHERWISE, we will invoke updateCell(), passing 'this' as argument as well as the cell's index
         changePlayer();                                     //change player each round
-        
-        if (checkWinner() !== false) {                           //invoke checkWinner(), !== false because last line of checkWinner()
-                changePlayer();                                 //this fixes bug of incorrect winner being displayed, have to change player again
-                gameStatus.textContent = `${currentPlayer} is the winner!`;
-        }                                      
+        checkWinner();
                                 
     }
 
-function updateCell(cell, index) {
-    gameboard[index] = currentPlayer;      //take the index of the gameboard, set it to current player - updating our placeholders
-    cell.textContent = currentPlayer;
+    function updateCell(cell, index) {
+        gameboard[index] = currentPlayer;      //take the index of the gameboard, set it to current player - updating our placeholders
+        cell.textContent = currentPlayer;
 
-}
-
-function changePlayer() {
-    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    gameStatus.textContent = `${currentPlayer}'s turn`;
-}
-
-
-function checkWinner() {                   
-    for(const condition of winCombinations) {
-        let [a, b, c] = condition
-
-        if(gameboard[a] && (gameboard[a] == gameboard[b] && gameboard[a] == gameboard[c])) {
-
-            return [a, b, c]
-        }
     }
-    return false;
-}
 
-function gameReset() {
-    currentPlayer = 'X';
-    gameboard = ['', '', '', '', '', '', '', '', '',];
-    gameStatus.textcontent = `${currentPlayer}'s turn`;
-    cells.forEach(cell => cell.textContent = '');
-    running = true;
-};
+    function changePlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        gameStatus.textContent = `${currentPlayer}'s turn`;
+    }
+
+
+    function checkWinner() {                           
+        for(const condition of winCombinations) {
+            let [a, b, c] = condition
+
+            if(gameboard[a] && (gameboard[a] == gameboard[b] && gameboard[a] == gameboard[c])) {
+                changePlayer();
+                gameStatus.textContent = `${currentPlayer} wins!`;
+                running = false;
+                return [a, b, c];
+            } else if (!gameboard.includes("")) {
+                gameStatus.textContent = 'Draw!!';
+                running = false;
+            }
+        }
+        return false;
+    }
+
+    function gameReset() {
+        currentPlayer = 'X';
+        gameStatus.textContent = `${currentPlayer}'s turn`;
+        gameboard = ['', '', '', '', '', '', '', '', '',];
+        cells.forEach(cell => cell.textContent = '');
+        running = true;
+    };
+
+   
 
 }) ();
 
 
 
-
-// const gameController = (() => {
-//     let players = [];
-//     let currentPlayerIndex = 0;
-//     let gameOver = false;
-
-//     const reset = () => {
-//             players = [
-//                 createPlayer(document.createElement())
-//             ]
-//     } 
-// }) ();
 
 
 
